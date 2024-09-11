632bf8b8c5a16ebd8a82810efa9d323e771747f0
andric_m <ivoandric1@gmail.com>
2024-09-10 20:56:09 -0400

    Add Main branch_ Python script, text file, and Jupyter Notebook for web scraping task

--- Task2_pythonscript.py
+++ Task2_pythonscript.py
@@ -75,5 +75,5 @@ if __name__ == '__main__':
 # 
 # 	# Instructions to Run this python code
 # 	# Give url as https://medium.com/@subashgandyer/papa-what-is-a-neural-network-c5e5cc427c7
-# task2 collaborator review
+
 
